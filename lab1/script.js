6057efc666dd0c7e236ea06b7c1cb618ce7fc94d
akiroaki <56996146+akiroaki@users.noreply.github.com>
2022-10-08 19:58:05 +0200

    Patterns, button added

---
+++ lab1/script.js
@@ -0,0 +1,14 @@
+$("#btn").click(function() {
+    $("#email").val("abc@gmail.com");
+    $("#post_code").val("87-100");
+    $("#nip").val("123-123-12-12");
+    $("#id_num").val("ABC100001")
+    $("#ipv4").val("111.11.111.11");
+    $("#web").val("https://weeia.p.lodz.pl/");
+    $("#path").val("C:\win")
+    $("#path2").val("C:\WindowS");
+    $("#path3").val("/etc/passwd");
+    $("#ipv6").val("");
+    $("#phone").val("123-123-123");
+  });
+  
