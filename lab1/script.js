49453faefbb07c12e9f6b2693e13c6f1148039d1
akiroaki <56996146+akiroaki@users.noreply.github.com>
2022-10-08 20:21:54 +0200

    Script moved to .html

--- lab1/script.js
+++ lab1/script.js
@@ -1,14 +1,2 @@
-function fill() {
-    document.getElementById('email').value="abc@gmail.com";
-    document.getElementById('post_code').value="87-100";
-    document.getElementById('nip').value="123-123-12-12";
-    document.getElementById('id_num').value="ABC100001";
-    document.getElementById('ipv4').value="198.12.121.11";
-    document.getElementById('web').value="https://weeia.p.lodz.pl/";
-    document.getElementById('path').value="C:\win";
-    document.getElementById('path2').value="C:\WinDowS";
-    document.getElementById('path3').value="/etc/passwd";
-    document.getElementById('ipv6').value="2001:db8::1428:57ab";
-    document.getElementById('phone').value="123-123-123";
-  }
+
   
