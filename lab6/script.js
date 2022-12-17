18b26aa163d802544bc171e073ad4c09ce789025
akiroaki <56996146+akiroaki@users.noreply.github.com>
2022-12-17 18:35:43 +0100

    wyszukiwanie bez przycisku

--- lab6/script.js
+++ lab6/script.js
@@ -188,6 +188,23 @@ function deleteClient(x) {
     document.getElementById('add_phone').value="";
   };
 }
+function mySearchingFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("search");
+  filter = input.value.toUpperCase();
+  tr = document.getElementClassName("clientList");
+  for (i = 0; i < tr.length-2; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
 
 function searchtable() {
   var clients = "";
